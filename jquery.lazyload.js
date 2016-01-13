70a3b9a01bc4d3049ca636e93717a52a3365ec3c
Perlover <perlover@perlover.com>
2016-01-13 10:48:07 +0100

    Added new event 'fullview'

--- jquery.lazyload.js
+++ jquery.lazyload.js
@@ -27,8 +27,8 @@
             container       : window,
             data_attribute  : "original",
             skip_invisible  : false,
-            toload          : null,
             appear          : null,
+            fullview        : null,
             load            : null,
             placeholder     : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"
         };
@@ -47,8 +47,8 @@
                 } else if (!$.belowthefold(this, settings) &&
                     !$.rightoffold(this, settings)) {
 			if ( $.inviewport(this, {threshold : 0}) )
-			    $this.trigger("appear");
-                        $this.trigger("toload");
+			    $this.trigger("fullview");
+                        $this.trigger("appear");
                         /* if we found an image we'll load, reset the counter */
                         counter = 0;
                 } else {
@@ -98,20 +98,20 @@
                 }
             }
 
-	    /* The appear event fires one time when image appears in viewport */
-	    $self.one("appear", function() {
-		this.appeared = true;
-		if (settings.appear)
-		    settings.appear.call(self, elements.length, settings);
+	    /* The fullview event to be fired one time when image has as full view in viewport */
+	    $self.one("fullview", function() {
+		this.fullviewed = true;
+		if (settings.fullview)
+		    settings.fullview.call(self, elements.length, settings);
 		/* Remove image from array so it is not looped next time. */
-		elements = $($.grep(elements, function(element) { return !element.loaded || !element.appeared; }));
+		elements = $($.grep(elements, function(element) { return !element.loaded || !element.fullviewed; }));
 	    });
 
-            /* When toload is triggered load original image. */
-            $self.one("toload", function() {
+            /* When appear is triggered load original image. */
+            $self.one("appear", function() {
                 if (!this.loaded) {
-                    if (settings.toload)
-                        settings.toload.call(self, elements.length, settings);
+                    if (settings.appear)
+                        settings.appear.call(self, elements.length, settings);
                     $("<img />")
                         .bind("load", function() {
 
@@ -127,7 +127,7 @@
                             self.loaded = true;
 
                             /* Remove image from array so it is not looped next time. */
-                            elements = $($.grep(elements, function(element) { return !element.loaded || !element.appeared; }));
+                            elements = $($.grep(elements, function(element) { return !element.loaded || !element.fullviewed; }));
 
                             if (settings.load)
                                 settings.load.call(self, elements.length, settings);
@@ -137,11 +137,11 @@
             });
 
             /* When wanted event is triggered load original image */
-            /* by triggering toload.                              */
+            /* by triggering appear.                              */
             if (0 !== settings.event.indexOf("scroll")) {
                 $self.bind(settings.event, function() {
                     if (!self.loaded) {
-                        $self.trigger("toload");
+                        $self.trigger("appear");
                     }
                 });
             }
@@ -158,7 +158,7 @@
             $window.bind("pageshow", function(event) {
                 if (event.originalEvent && event.originalEvent.persisted) {
                     elements.each(function() {
-                        $(this).trigger("toload");
+                        $(this).trigger("appear");
                     });
                 }
             });
@@ -187,6 +187,18 @@
         return fold <= $(element).offset().top - settings.threshold;
     };
 
+    $.fullviewfrombelow = function(element, settings) {
+        var fold;
+
+        if (settings.container === undefined || settings.container === window) {
+            fold = (window.innerHeight ? window.innerHeight : $window.height()) + $window.scrollTop();
+        } else {
+            fold = $(settings.container).offset().top + $(settings.container).height();
+        }
+
+        return $(element).offset().top + $(element).height() < fold;
+    };
+
     $.rightoffold = function(element, settings) {
         var fold;
 
@@ -197,6 +209,18 @@
         }
 
         return fold <= $(element).offset().left - settings.threshold;
+    };
+
+    $.fullviewfromright = function(element, settings) {
+        var fold;
+
+        if (settings.container === undefined || settings.container === window) {
+            fold = $window.width() + $window.scrollLeft();
+        } else {
+            fold = $(settings.container).offset().left + $(settings.container).width();
+        }
+
+        return $(element).offset().left + $(element).width() < fold;
     };
 
     $.abovethetop = function(element, settings) {
@@ -211,6 +235,18 @@
         return fold >= $(element).offset().top + settings.threshold  + $(element).height();
     };
 
+    $.fullviewfromtop = function(element, settings) {
+        var fold;
+
+        if (settings.container === undefined || settings.container === window) {
+            fold = $window.scrollTop();
+        } else {
+            fold = $(settings.container).offset().top;
+        }
+
+        return $(element).offset().top >= fold;
+    };
+
     $.leftofbegin = function(element, settings) {
         var fold;
 
@@ -223,25 +259,43 @@
         return fold >= $(element).offset().left + settings.threshold + $(element).width();
     };
 
+    $.fullviewfromleft = function(element, settings) {
+        var fold;
+
+        if (settings.container === undefined || settings.container === window) {
+            fold = $window.scrollLeft();
+        } else {
+            fold = $(settings.container).offset().left;
+        }
+
+        return $(element).offset().left >= fold;
+    };
+
     $.inviewport = function(element, settings) {
          return !$.rightoffold(element, settings) && !$.leftofbegin(element, settings) &&
                 !$.belowthefold(element, settings) && !$.abovethetop(element, settings);
-     };
+    };
+
+    $.fullviewinviewport = function(element, settings) {
+         return $.fullviewfromright(element, settings) && $.fullviewfromleft(element, settings) &&
+                $.fullviewfrombelow(element, settings) && $.fullviewfromtop(element, settings);
+    };
 
     /* Custom selectors for your convenience.   */
     /* Use as $("img:below-the-fold").something() or */
     /* $("img").filter(":below-the-fold").something() which is faster */
 
     $.extend($.expr[":"], {
-        "below-the-fold" : function(a) { return $.belowthefold(a, {threshold : 0}); },
-        "above-the-top"  : function(a) { return !$.belowthefold(a, {threshold : 0}); },
-        "right-of-screen": function(a) { return $.rightoffold(a, {threshold : 0}); },
-        "left-of-screen" : function(a) { return !$.rightoffold(a, {threshold : 0}); },
-        "in-viewport"    : function(a) { return $.inviewport(a, {threshold : 0}); },
+        "below-the-fold"        : function(a) { return $.belowthefold(a, {threshold : 0}); },
+        "above-the-top"         : function(a) { return !$.belowthefold(a, {threshold : 0}); },
+        "right-of-screen"       : function(a) { return $.rightoffold(a, {threshold : 0}); },
+        "left-of-screen"        : function(a) { return !$.rightoffold(a, {threshold : 0}); },
+        "in-viewport"           : function(a) { return $.inviewport(a, {threshold : 0}); },
+        "full-view-in-viewport" : function(a) { return $.fullviewinviewport(a); },
         /* Maintain BC for couple of versions. */
-        "above-the-fold" : function(a) { return !$.belowthefold(a, {threshold : 0}); },
-        "right-of-fold"  : function(a) { return $.rightoffold(a, {threshold : 0}); },
-        "left-of-fold"   : function(a) { return !$.rightoffold(a, {threshold : 0}); }
+        "above-the-fold"        : function(a) { return !$.belowthefold(a, {threshold : 0}); },
+        "right-of-fold"         : function(a) { return $.rightoffold(a, {threshold : 0}); },
+        "left-of-fold"          : function(a) { return !$.rightoffold(a, {threshold : 0}); }
     });
 
 })(jQuery, window, document);
